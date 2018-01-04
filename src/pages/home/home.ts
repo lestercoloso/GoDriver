025b45d957499dcd099540966d43a0a895a9d921
lester.coloso <lester.coloso@mdscsi.com>
2018-01-04 18:49:35 +0800

    initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,73 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+ 
+declare var google;
+ 
+@Component({
+  selector: 'home-page',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  pickup: any;
+  dropoff: any;
+ 
+ 
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+ 
+  }
+ 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    this.geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 19,
+        mapTypeId: 'roadmap'
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      console.log(err);
+    });
+ 
+  }
+
+  addMarker(){
+ 
+  let marker = new google.maps.Marker({
+    map: this.map,
+    animation: google.maps.Animation.DROP,
+    position: this.map.getCenter()
+  });
+ 
+  let content = "<h4>Information!</h4>";         
+ 
+  this.addInfoWindow(marker, content);
+ 
+}
+
+addInfoWindow(marker, content){
+ 
+  let infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+ 
+  google.maps.event.addListener(marker, 'click', () => {
+    infoWindow.open(this.map, marker);
+  });
+ 
+}
+ 
+}
